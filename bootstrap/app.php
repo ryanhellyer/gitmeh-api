9a55de2b0b2287b5679c73c87a26b9d26b3b6e9a
Ryan Hellyer <ryanhellyer@gmail.com>
2026-04-13 20:24:46 +0200

    Set production environment configuration and HTTPS URL.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: env('TRUSTED_PROXIES', '*'));
+
         $middleware->validateCsrfTokens(except: [
             'gitmeh',
             'gitmeh/*',
