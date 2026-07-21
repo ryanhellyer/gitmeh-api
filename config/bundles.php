f128edf0d4d14b36695cb7f7870e84cad73f8e26
Ryan <ryan@hellyer.kiwi>
2026-07-21 15:09:01 +0200

    feat(deps): add production-stats bundle and update dependency versions

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
+    RyanHellyer\ProductionStats\Symfony\ProductionStatsBundle::class => ['all' => true],
 ];
